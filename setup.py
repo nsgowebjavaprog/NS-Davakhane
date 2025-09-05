db98329050c70f0cb4141d3181f5d724463c3da5
ns loni <nsloni@gmail.com>
2025-09-05 11:04:38 +0530

    Setup for Project

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="nagarajloni209 Das",
+    author_email="nagarajloni209@gmail.com",
+    packages=find_packages()
+)
